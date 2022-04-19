0e9d96406e442f463583bd96cc6fda4c11faf04d
lou0124 <63856928+lou0124@users.noreply.github.com>
2022-04-19 15:44:09 +0900

    Update 이진_탐색_재귀.c

--- 이진_탐색_재귀.c
+++ 이진_탐색_재귀.c
@@ -4,6 +4,7 @@
 void insertSort(int* arr, int n)
 {
 	int i, j, temp, ind;
+	
 	for (i = 1;i < n;i++)
 	{
 		ind = i;
@@ -24,12 +25,15 @@ void insertSort(int* arr, int n)
 int binarySearch(int* arr, int k, int l, int r)
 {
 	int mid = (l + r) / 2;
+	
 	if (k < arr[0]) return -1;
+	
 	if (l > r) 
 	{
 		while (arr[mid] > k ) mid--;
 		return mid;
 	}
+	
 	if (k == arr[mid]) return mid;
 	else if (k < arr[mid]) return binarySearch(arr, k, l, mid - 1);
 	else return binarySearch(arr, k, mid + 1, r);
@@ -40,12 +44,14 @@ int main()
 	int n, k, i, * arr;
 	scanf("%d %d", &n, &k);
 	arr = (int*)malloc(sizeof(int) * n);
+	
 	for (i = 0;i < n;i++) 
-  {
+  	{
 		scanf("%d", &arr[i]);
 	}
 	insertSort(arr, n);
 	printf(" %d", binarySearch(arr, k, 0, n - 1));
+	
 	free(arr);
 	return 0;
 }
